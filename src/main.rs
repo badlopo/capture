52a29ed86ef2a70e0e6ac604c36708f951487b41
lopo <lopo@zju.edu.cn>
2024-04-09 23:52:08 +0800

    test: misc test

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,24 @@ mod snapper;
 fn main() {
     cropper::Cropper::ui_test();
 }
+
+#[cfg(test)]
+mod misc_test {
+    use std::io::{Cursor, Read};
+    use image;
+    use image::{DynamicImage, RgbaImage};
+
+    #[test]
+    fn image_read_test() {
+        // let img = image::load_from_memory(include_bytes!("../test/black.png")).unwrap();
+        // println!("Image: {:?}", img);
+
+        // let img = image::open("./test/black.png").unwrap().into_rgba8();
+        // println!("Image: {:?}", img.into_raw());
+
+        // let buffer = vec![];
+        // let mut buffer = Cursor::new(buffer);
+        // img.write_to(&mut buffer, image::ImageFormat::Png).unwrap();
+        // println!("Image: {:?}", buffer);
+    }
+}
