b83a8855887bcdbfde3a71f7a174540de138113e
lopo <lopo@zju.edu.cn>
2024-04-09 23:47:10 +0800

    todo: ..

--- src/cropper/mod.rs
+++ src/cropper/mod.rs
@@ -2,16 +2,18 @@ mod app;
 
 use egui::ViewportBuilder;
 use crate::cropper::app::MyApp;
+use crate::snapper::Snapper;
 
 pub struct Cropper;
 
 impl Cropper {
-    pub fn load_and_crop() {
-        // TODO
-    }
+    /// take a snapshot and crop it with interactive UI
+    pub fn snap_and_crop() -> Result<(), String> {
+        let source = Snapper::take_snapshot()?;
 
-    pub fn crop() {
-        // TODO
+        // TODO: crop the source image
+
+        Ok(())
     }
 
     /// test only, use for not expose too much to outside mod
@@ -20,7 +22,7 @@ impl Cropper {
             viewport: ViewportBuilder::default()
                 .with_position([5.0, 5.0])
                 .with_inner_size([4470.0, 2510.0])
-                .with_clamp_size_to_monitor_size(false)
+                // .with_clamp_size_to_monitor_size(false)
                 .with_decorations(false)
                 .with_always_on_top(),
             ..Default::default()
