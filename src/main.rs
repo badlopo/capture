dd635dc81a0d1f2f205298f128edecdcc51b8223
lopo <lopo@zju.edu.cn>
2024-05-03 16:05:25 +0800

    chore: drop test

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,3 @@ mod snapper;
 fn main() {
     cropper::Cropper::exec(false).unwrap();
 }
-
-#[cfg(test)]
-mod misc_test {
-    use std::io::{Cursor, Read};
-    use image;
-    use image::{DynamicImage, RgbaImage};
-
-    #[test]
-    fn image_read_test() {
-        // let img = image::load_from_memory(include_bytes!("../test/black.png")).unwrap();
-        // println!("Image: {:?}", img);
-
-        // let img = image::open("./test/black.png").unwrap().into_rgba8();
-        // println!("Image: {:?}", img.into_raw());
-
-        // let buffer = vec![];
-        // let mut buffer = Cursor::new(buffer);
-        // img.write_to(&mut buffer, image::ImageFormat::Png).unwrap();
-        // println!("Image: {:?}", buffer);
-    }
-}
