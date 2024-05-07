302b4bbdcf187a429144f0501e693588f54c17fe
lopo <lopo@zju.edu.cn>
2024-05-07 23:37:11 +0800

    chore: real main

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,47 @@ mod canonical;
 mod cropper;
 mod snapper;
 
-// fn main() {
-//     cropper::Cropper::exec(Default::default()).unwrap();
-// }
+fn main() {
+    cropper::Cropper::exec(Default::default()).unwrap();
+}
 
 // 窗口大于屏幕时, resize 会导致窗口被剪切为屏幕大小 (突变)
-fn main() {
-    use eframe::epaint::Color32;
-    use egui::{Frame, Key, ViewportBuilder, ViewportCommand};
-
-    let option = eframe::NativeOptions {
-        viewport: ViewportBuilder::default()
-            .with_decorations(true)
-            .with_position([10.0, 10.0])
-            .with_inner_size([4000.0, 3000.0]),
-        ..Default::default()
-    };
-
-    eframe::run_simple_native(
-        "my-app",
-        option,
-        |ctx, _frame| {
-            egui::CentralPanel::default()
-                .frame(Frame::none().fill(Color32::WHITE))
-                .show(ctx, |ui| {
-                    if ui.button("close").clicked() {
-                        ctx.send_viewport_cmd(ViewportCommand::Close);
-                    }
-
-                    if ctx.input(|i| i.key_pressed(Key::Escape)) {
-                        ctx.send_viewport_cmd(ViewportCommand::Close);
-                    }
-
-                    // 空格键居中
-                    if ctx.input(|i| i.key_pressed(Key::Space)) {
-                        ctx.send_viewport_cmd(ViewportCommand::center_on_screen(ctx).unwrap());
-                        println!("{:?}", ctx.screen_rect());
-                    }
-                });
-        },
-    ).unwrap();
-}
+// fn main() {
+//     use eframe::epaint::Color32;
+//     use egui::{Frame, Key, ViewportBuilder, ViewportCommand};
+//
+//     let option = eframe::NativeOptions {
+//         viewport: ViewportBuilder::default()
+//             .with_decorations(true)
+//             .with_position([10.0, 10.0])
+//             .with_inner_size([4000.0, 3000.0]),
+//         ..Default::default()
+//     };
+//
+//     eframe::run_simple_native(
+//         "my-app",
+//         option,
+//         |ctx, _frame| {
+//             egui::CentralPanel::default()
+//                 .frame(Frame::none().fill(Color32::WHITE))
+//                 .show(ctx, |ui| {
+//                     if ui.button("close").clicked() {
+//                         ctx.send_viewport_cmd(ViewportCommand::Close);
+//                     }
+//
+//                     if ctx.input(|i| i.key_pressed(Key::Escape)) {
+//                         ctx.send_viewport_cmd(ViewportCommand::Close);
+//                     }
+//
+//                     // 空格键居中
+//                     if ctx.input(|i| i.key_pressed(Key::Space)) {
+//                         ctx.send_viewport_cmd(ViewportCommand::center_on_screen(ctx).unwrap());
+//                         println!("{:?}", ctx.screen_rect());
+//                     }
+//                 });
+//         },
+//     ).unwrap();
+// }
 
 // https://github.com/emilk/egui/issues/4458
 // fn main() {
